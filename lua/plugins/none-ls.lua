e08201aa677105c8aa3b742b4b1c1a2afd935c88
Shubham Mistry <mistryshubham29@gmail.com>
2025-01-12 14:53:05 +0530

    initial commit

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,52 @@
+return {
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+    "jayp0521/mason-null-ls.nvim", -- ensure dependencies are installed
+  },
+  config = function()
+    local null_ls = require("null-ls")
+    local formatting = null_ls.builtins.formatting -- to setup formatters
+    local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+
+    -- Formatters & linters for mason to install
+    require("mason-null-ls").setup({
+      ensure_installed = {
+        "prettier", -- JS/TS/HTML/JSON formatter
+        "eslint_d", -- JS/TS linter
+        "stylua", -- Lua formatter
+        "shfmt", -- Shell formatter
+        "markdownlint", -- Markdown linter
+        "jsonlint", -- JSON linter
+      },
+      automatic_installation = true,
+    })
+
+    local sources = {
+      formatting.prettier.with({ filetypes = { "javascript", "typescript", "tsx", "json", "yaml", "html", "markdown", "css" } }),
+--      diagnostics.eslint_d,
+      --formatting.prettier.with({ filetypes = { "html", "json", "yaml", "markdown" } }),
+      formatting.stylua,
+      formatting.shfmt.with({ args = { "-i", "4" } }),
+    }
+
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    null_ls.setup({
+      -- debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
+      sources = sources,
+      -- you can reuse a shared lspconfig on_attach callback here
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ async = false })
+            end,
+          })
+        end
+      end,
+    })
+  end,
+}
