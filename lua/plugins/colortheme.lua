03d7693564e0ae90148a8901da54c97394ac777f
Shubham Mistry <mistryshubham29@gmail.com>
2025-01-15 14:27:02 +0530

    change color theme to onedark

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,28 +1,25 @@
 return {
-  "shaunsingh/nord.nvim",
+  "navarasu/onedark.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    -- Example config in lua
-    vim.g.nord_contrast = true
-    vim.g.nord_borders = false
-    vim.g.nord_disable_background = true
-    vim.g.nord_italic = false
-    vim.g.nord_uniform_diff_background = true
-    vim.g.nord_bold = false
+    local bg = true
 
-    -- Load the colorscheme
-    require("nord").set()
-
-    -- Toggle background transparency
-    local bg_transparent = true
+    require("onedark").setup({
+      style = "darker",
+      transparent = bg, -- Show/hide background
+      term_colors = true, -- Change terminal color as per the selected theme style
+      ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+    })
+    require("onedark").load()
 
     local toggle_transparency = function()
-      bg_transparent = not bg_transparent
-      vim.g.nord_disable_background = bg_transparent
-      vim.cmd([[colorscheme nord]])
+      bg = not bg
+      require("onedark").setup({
+        transparent = bg, -- Show/hide background
+      })
+      require("onedark").load()
     end
-
     vim.keymap.set("n", "<leader>bg", toggle_transparency, { noremap = true, silent = true })
   end,
 }
