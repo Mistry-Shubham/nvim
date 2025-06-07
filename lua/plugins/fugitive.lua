902a4f127b0951a4a6a8bbb9b4324acf0732d335
Shubham Mistry <mistryshubham29@gmail.com>
2025-06-07 23:10:55 +0530

    add cursor animation and fugitive keymaps change

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,8 +2,8 @@ return {
   "tpope/vim-fugitive",
   event = "VeryLazy",
   keys = {
-    { "<leader>gd", "<cmd>Gvdiffsplit<CR>", desc = "Git Diff (vsplit)" },
-    { "<leader>gD", "<cmd>Gdiffsplit<CR>", desc = "Git Diff" },
+    { "<leader>gd", "<cmd>Gvdiffsplit!<CR>", desc = "Git Diff (vsplit)" },
+    { "<leader>gD", "<cmd>Gdiffsplit!<CR>", desc = "Git Diff" },
     { "<leader>gB", "<cmd>Git blame<CR>", desc = "Git blame all line" },
   },
 }
