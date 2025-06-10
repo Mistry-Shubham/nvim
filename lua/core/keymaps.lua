a36877d82c6749ec86686de52685be087737879b
Shubham Mistry <mistryshubham29@gmail.com>
2025-06-10 22:32:42 +0530

    add new keymaps for quick fix list and split screen naigation

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -25,10 +25,18 @@ keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) 
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
+keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to down split" })
+keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to up split" })
+keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left split" })
+keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right split" })
+
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
-vim.keymap.set("n", "<leader>yd", functions.copy_diagnostic_under_cursor, { desc = "Copy diagnostic under cursor" })
+keymap.set("n", "<leader>yd", functions.copy_diagnostic_under_cursor, { desc = "Copy diagnostic under cursor" })
+
+keymap.set("n", "]q", "<cmd>cn<CR>", { desc = "Next item in quick fix list" })
+keymap.set("n", "[q", "<cmd>cp<CR>", { desc = "Perv item in quick fix list" })
