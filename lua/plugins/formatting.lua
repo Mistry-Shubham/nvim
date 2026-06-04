451143bf6339335b6a315cee645026514819d7d5
Shubham Mistry <mistryshubham29@gmail.com>
2026-06-04 21:25:43 +0530

    fix: treesitter and comment plugin

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -19,6 +19,7 @@ return {
         markdown = { "prettier" },
         lua = { "stylua" },
         python = { "isort", "black" },
+        rust = { "rustfmt" },
       },
       format_on_save = {
         lsp_fallback = lsp_fallback,
--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -2,7 +2,7 @@ return {
   "iamcco/markdown-preview.nvim",
   event = "VeryLazy",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && npm install",
+  build = "cd app && npm install && git checkout -- yarn.lock",
   init = function()
     vim.g.mkdp_filetypes = { "markdown" }
   end,
